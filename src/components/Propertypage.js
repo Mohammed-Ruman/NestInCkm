d9995f0561a2ad685104c5c7827b6223f5040969
Mohammed Ruman <mohammedruman25@gmail.com>
2023-09-13 22:24:44 +0530

    Worked on propertyPage, added room card

--- src/components/Propertypage.js
+++ src/components/Propertypage.js
@@ -1,14 +1,134 @@
-import React from 'react'
-import {  useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import propertyJson from '../JsonData/PremiumHomestay.json';
+import RoomCard from './RoomCard';
 
 function Propertypage() {
+  const { propertyId } = useParams();
+  const [propertyDetails, setPropertyDetails] = useState(null);
 
-    const paramId=useParams();
-    
+ 
+
+  useEffect(() => {
+    // Find the property details based on propertyId
+    const foundProperty = propertyJson?.find(property => property.propertyId === propertyId);
+
+    if (foundProperty) {
+      // If property is found, set it in the state
+      setPropertyDetails(foundProperty);
+      console.log(foundProperty);
+    } else {
+      // Handle the case when property is not found
+      // You can set an error message or redirect to a 404 page
+      console.error(`Property with id ${propertyId} not found.`);
+    }
+  }, [propertyId]);
+
+  if (!propertyDetails) {
+    // You can render a loading indicator while the data is being fetched
+    return <div>Invalid Property Id : Property details not available</div>;
+  }
 
   return (
-    <div>{paramId.propertyId}</div>
-  )
+    <div className="container text-center">
+    <img
+      className="img-fluid mx-auto mt-3"
+      src={propertyDetails.coverImageUrl}
+      alt="Cover"
+      style={{ maxWidth: '100%', height: 'auto' }}
+    />
+    <div className="container mt-3 mb-3">
+      <h4 className='text-center'>Homestay : {propertyDetails.propertyId}</h4>
+      <div className="row">
+        <div className="col-md-8">
+          <div className="row ">
+            <p className='text-start' > Price :{`₹ `}{propertyDetails.Price}</p>
+          </div>
+          <div className="row">
+            <p className='text-start'>Package Details : {propertyDetails.packageDetails}</p>
+          </div>
+
+        </div>
+        <div className="col-md-4">
+          <Link to='https://www.google.com' className='btn btn-primary'>Book</Link>
+        </div>
+      </div>
+
+    </div>
+
+    <div className="container text-start">
+    {Object.entries(propertyDetails?.specialFeatures).map(([key, value], index) => (
+        <p key={index} className='fw-bold'>
+          <span> | </span> {value}
+        </p>
+    ))}
+    </div>
+
+    <div className="container mt-3">
+          <h2 className="mx-auto text-center mt-3 mb-3 p-3 rounded-5" style={{backgroundColor:"rgb(153 106 37 / 52%)"}}>Rooms</h2>
+            <div className="row row-cols-1 row-cols-md-2 g-4">
+            {propertyDetails?.rooms.map((room,index) => {
+                return (
+                  <div className="col mb-4" key={index}>
+                    <RoomCard roomInfo={room} />
+                  </div>
+                );
+              })}
+            </div>
+    </div>
+    
+    <div className="container mt-3">
+      <h2 className="mx-auto text-center mt-3 mb-3 p-3 ">Image Gallery</h2>
+      {propertyDetails && (
+        <div className="row">
+          {propertyDetails.galleryImageUrls.map((imageUrl, index) => (
+            <div key={index} className="col-md-4 mb-4">
+              <img
+                src={imageUrl}
+                alt=""
+                className="img-fluid"
+              />
+              {/* You can add additional content or captions here */}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+
+    <div className="container mt-3">
+      <div className="text-center mt-3 mb-3">
+        <h2 className="mx-auto mb-0">Terms and Condition</h2>
+        <hr className="w-100 mx-auto mt-1 mb-3" style={{ borderColor: 'black' }} />
+      </div>
+
+          
+      <ul className='text-start'>
+        <li>Alcohol will not be served.</li>
+        <li>Pets not allowed.</li>
+      </ul>
+
+      <h5 className="mx-auto text-start  ">BOOKING POLICY</h5>
+      <ul className='text-start'>
+        <li>30% Advance needs to be paid to confirm the booking.</li>
+        <li>Balance payment to be paid upon arrival.</li>
+      </ul>
+
+      <h5 className="mx-auto text-start  ">CANCELLATION POLICY</h5>
+      <ul className='text-start'>
+        <li>Full refund if cancelled before 15 days of Check-in.</li>
+        <li>50% refund if cancelled before 10 days of Check-in.</li>
+        <li>No refund if cancelled before 7 days of Check-in.</li>
+      </ul>
+    </div>
+
+
+
+
+
+  
+  </div>
+  
+  );
 }
 
-export default Propertypage
+export default Propertypage;
---
+++ src/components/RoomCard.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+function RoomCard({roomInfo}) {
+  
+  const id = `roomCarousel-${roomInfo.roomName.replace(/\s/g, '')}`;
+  console.log(`#${id}`);
+    return (
+    <>
+    <div className="card">
+      <div className="card-body text-center">
+        <h4 className="card-title mb-2">{roomInfo.roomName}</h4>
+      </div>
+
+      <div className="container">
+      <div id={id} className="carousel slide" data-bs-ride="carousel">
+        <ol className="carousel-indicators">
+          {roomInfo?.roomImageUrls.map((_, index) => (
+            <li
+              key={index}
+              data-bs-target={`#${id}`}
+              data-bs-slide-to={index}
+              className={index === 0 ? 'active' : ''}
+            ></li>
+          ))}
+        </ol>
+        <div className="carousel-inner">
+          {roomInfo?.roomImageUrls.map((image, index) => (
+            <div
+              key={index}
+              className={`carousel-item ${index === 0 ? 'active' : ''}`}
+            >
+              <img
+                src={image}
+                alt=''
+                className="d-block w-100"
+              />
+            </div>
+          ))}
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target={`#${id}`}
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target={`#${id}`}
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+
+
+      
+      <div className="card-body text-center">
+        <h6 className="card-title " style={{marginBottom:"0"}}>{roomInfo.description}</h6>
+      </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default RoomCard
